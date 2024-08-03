d063e055ad8232fb445dcfc91dc401b73186c8a7
Darsh Patel <darshanpatel1902@gmail.com>
2024-08-03 11:05:30 -0700

    added json and script

--- next.config.js
+++ next.config.js
@@ -6,6 +6,8 @@ const nextConfig = {
   reactStrictMode: true,
   content: [
     './src/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
     './public/index.html',
   ],
   theme: {
