bb777ac4c0cde0169790df95362dfcb4066760ee
System Administrator <root@Darshans-MBP.lan>
2024-08-08 09:44:30 -0700

    addeed build bbb...

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import config from "@config/config.json";
 import theme from "@config/theme.json";
 import Head from "next/head";
+import Script from "next/script"; // Import Script component
 import { useEffect, useState } from "react";
 import TagManager from "react-gtm-module";
 import "styles/style.scss";
@@ -36,16 +37,6 @@ const App = ({ Component, pageProps }) => {
   return (
     <>
       <Head>
-        {/* GTM script */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','${config.params.tag_manager_id}');`,
-          }}
-        />
         {/* Google font css */}
         <link
           rel="preconnect"
@@ -63,6 +54,18 @@ const App = ({ Component, pageProps }) => {
           content="width=device-width, initial-scale=1, maximum-scale=5"
         />
       </Head>
+      {/* GTM script */}
+      <Script
+        id="gtm-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','${config.params.tag_manager_id}');`,
+        }}
+      />
       <noscript>
         <iframe
           src={`https://www.googletagmanager.com/ns.html?id=${config.params.tag_manager_id}`}
